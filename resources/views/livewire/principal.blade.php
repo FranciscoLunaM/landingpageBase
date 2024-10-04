9ab71c40b22a8544453e44747cf2a82ccf0625e2
Francisco Enrique <franciscoe.martinez@hotmail.com>
2024-10-04 11:03:31 -0600

    terminado toda la base

--- resources/views/livewire/principal.blade.php
+++ resources/views/livewire/principal.blade.php
@@ -2,28 +2,25 @@
    
     <div name="Container" class="flex flex-col justify-center items-center w-full text-white lg:flex-row lg:px-10">
         
-            <div class=" lg:w-[50%] lg:pl-[10%] lg:pb-0 pb-10 pt-20 w-full">
+            <div class=" pb-10 pt-20 w-full justify-center items-center text-center flex flex-col lg:text-start lg:items-start lg:w-[50%] lg:pl-[10%] lg:pb-0 ">
                 <h1 class="text-9xl text-[#f4ff5d] mb-3 motion-safe:animate-bounce"  ><strong>Brilla</strong></h1>
-                <h2 class="text-7xl	font-medium mb-3	">Con cada instante</h2>
-                <h3 class="font-light text-3xl	">joyas que cuentan tu historia</h3>
+                <h2 class="text-7xl	font-medium mb-3">Con cada instante</h2>
+                <h3 class="font-light text-3xl">joyas que cuentan tu historia</h3>
 
                 <div name="media" class="pt-5 flex">
-                    <a>
-                        @svg('bi-facebook')
-                      </a>
-                    <a class="pr-5" href="">#</a>
-                    <a class="pr-5" href="">#</a>
-                    <a class="pr-5" href="">#</a>
+                    <a href="#"> <x-bi-facebook class="w-12 h-12 hover:scale-125 ease-in duration-200"/></a>
+                    <a class="px-10" href="#"><x-bi-instagram class="w-12 h-12 hover:scale-125 ease-in duration-200"/></a>
+                    <a class="pr-10" href="#"><x-bi-youtube class="w-12 h-12 hover:scale-125 ease-in duration-200"/></a>
                 </div>
             </div>
-            <div name="image" class="lg:w-[50%] w-full">
-                <img class="h-[600px] w-156" src="{{URL('images/model1.png')}}" alt="Image">
+            <div name="image" class="lg:w-[50%] w-full flex">
+                <img class="lg:h-[600px] lg:w-156 " src="{{URL('images/model1.png')}}" alt="Image">
             </div>
        
     </div>
 
     
-    <div class="w-full flex justify-center items-center pt-36 pb-36 hover:bg-slate-300 ease-in duration-500">
+    <div class="w-full flex justify-center items-center pt-36 pb-36 bg-slate-300 lg:bg-[#1f242d] lg:hover:bg-slate-300 ease-in duration-500">
         <button class="	" ><img class="h-[150px]" src="{{URL('images/logo.png')}}" alt=""></button>
     </div>
 
@@ -49,13 +46,13 @@
         <h1 class="text-7xl text-white font-medium italic">Contacto</h1>
             <div class="items-center justify-center w-full lg:w-[50%] ">
                 <div class="py-1  ">
-                    <x-input placeholder="Tu nombre" icon="o-user" class="text-white  bg-slate-700 border-[#1f242d]	" />
+                    <x-input placeholder="Tu nombre" icon="o-user" class="text-white  bg-slate-700 border-[#1f242d]" />
                 </div >
                 <div class="py-1">
-                    <x-input placeholder="Email" icon="c-at-symbol" class="text-white bg-slate-700 border-[#1f242d]	" />
+                    <x-input placeholder="Email" icon="c-at-symbol" class="text-white bg-slate-700 border-[#1f242d]" />
                 </div>
                 <div class="py-1">
-                    <x-textarea class="text-white  bg-slate-700 border-[#1f242d]	"
+                    <x-textarea class="text-white  bg-slate-700 border-[#1f242d]"
                     label="Mensaje"
                     placeholder=""
                     rows="5"
